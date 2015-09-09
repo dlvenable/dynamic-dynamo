0ca769409743af543ce9f1bcca92f0150eb95e0f
David Venable <david@venable.io>
2015-09-08 19:48:54 -0500

    Improved testing of the TableHelper.

--- src/test/java/io/venable/amazonaws/dynamo/table/TableHelperTest.java
+++ src/test/java/io/venable/amazonaws/dynamo/table/TableHelperTest.java
@@ -17,13 +17,16 @@
 package io.venable.amazonaws.dynamo.table;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.model.DescribeTableRequest;
-import com.amazonaws.services.dynamodbv2.model.DescribeTableResult;
-import com.amazonaws.services.dynamodbv2.model.TableDescription;
-import com.amazonaws.services.dynamodbv2.model.TableStatus;
+import com.amazonaws.services.dynamodbv2.model.*;
+import io.venable.amazonaws.dynamo.table.builder.TableBuilder;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
+import java.util.Objects;
 import java.util.UUID;
 
 import static org.mockito.Matchers.any;
@@ -41,15 +44,52 @@ public class TableHelperTest
         tableDefiner = mock(TableDefiner.class);
     }
 
-    private void stubTableToExistActive()
+    private static class DescribeTableMatcher extends BaseMatcher<DescribeTableRequest>
+    {
+        private final String tableName;
+
+        public DescribeTableMatcher(String tableName)
+        {
+            Objects.requireNonNull(tableName);
+            this.tableName = tableName;
+        }
+
+        @Override
+        public boolean matches(Object item)
+        {
+            if(! (item instanceof DescribeTableRequest))
+                return false;
+
+            DescribeTableRequest describeTableRequest = (DescribeTableRequest) item;
+
+            return tableName.equals(describeTableRequest.getTableName());
+        }
+
+        @Override
+        public void describeTo(Description description)
+        { }
+    }
+
+    private static DescribeTableMatcher requestsToDescribeTable(String tableName)
+    {
+        return new DescribeTableMatcher(tableName);
+    }
+
+    private void stubTableToExistActive(String tableName)
     {
         TableDescription tableDescription = mock(TableDescription.class);
+        stub(tableDescription.getTableName()).toReturn(tableName);
         stub(tableDescription.getTableStatus()).toReturn(TableStatus.ACTIVE.toString());
 
         DescribeTableResult describeTableResult = mock(DescribeTableResult.class);
         stub(describeTableResult.getTable()).toReturn(tableDescription);
 
-        stub(amazonDynamoDB.describeTable(any(DescribeTableRequest.class))).toReturn(describeTableResult);
+        stub(amazonDynamoDB.describeTable(argThat(requestsToDescribeTable(tableName)))).toReturn(describeTableResult);
+    }
+
+    private void stubTableToNotExist(String tableName)
+    {
+        when(amazonDynamoDB.describeTable(argThat(requestsToDescribeTable(tableName)))).thenThrow(ResourceNotFoundException.class);
     }
 
     @Test
@@ -57,11 +97,34 @@ public class TableHelperTest
     {
         String tableName = UUID.randomUUID().toString();
 
-        stubTableToExistActive();
+        stubTableToExistActive(tableName);
 
         for(int i = 0; i < 5; i++)
             TableHelper.createTableIfNecessary(amazonDynamoDB, tableName, tableDefiner);
 
         verify(amazonDynamoDB, times(1)).describeTable(any(DescribeTableRequest.class));
+    }
+
+    @Test
+    public void createTableIfNecessary_should_create_a_table_if_it_does_not_exist()
+    {
+        String tableName = UUID.randomUUID().toString();
+
+        stubTableToNotExist(tableName);
+
+        doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                TableBuilder tableBuilder = (TableBuilder) invocationOnMock.getArguments()[0];
+
+                tableBuilder.primary().hash().name(UUID.randomUUID().toString()).type(ScalarAttributeType.S);
+                tableBuilder.primary().readCapacity(1).writeCapacity(1);
+                return null;
+            }
+        }).when(tableDefiner).defineTable(any(TableBuilder.class));
+
+        TableHelper.createTableIfNecessary(amazonDynamoDB, tableName, tableDefiner);
+
+        verify(amazonDynamoDB).createTable(any(CreateTableRequest.class));
     }
 }
